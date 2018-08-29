be1378de828964ea248c966c723b31a1bfed92eb
Lars Nielsen <lnc13.lars@gmail.com>
2018-08-29 09:10:44 +0200

    Added run benckmark script

---
+++ RUN_benchmark_block_coder.sh
@@ -0,0 +1,16 @@
+./build/linux/benchmark_block_coder ./test_config/536870912_m_10_1_8_8_16_32.csv ./results
+sleep 10
+./build/linux/benchmark_block_coder ./test_config/1073741824_g_1000_1_8_8_16_32.csv ./results
+sleep 10
+./build/linux/benchmark_block_coder ./test_config/csv_test_config.csv ./results
+sleep 10
+./build/linux/benchmark_block_coder ./test_config/104857600_m_1000_1_8_8_16_32.csv ./results
+sleep 10
+./build/linux/benchmark_block_coder ./test_config/536870912_m_1000_1_8_8_16_32.csv ./results
+sleep 10
+./build/linux/benchmark_block_coder ./test_config/536870912_m_1_1_8_8_16_32.csv ./results
+sleep 10
+./build/linux/benchmark_block_coder ./test_config/1048576_m_1000_1_8_8_16_32.csv ./results
+sleep 10
+./build/linux/benchmark_block_coder ./test_config/10485760_m_1000_1_8_8_16_32.csv ./results
+sleep 10

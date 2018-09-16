dcfbefc8114d8882f44b2d0cf891ee2e2484a849
Lars Nielsen <lnc13.lars@gmail.com>
2018-09-16 12:34:55 +0200

    new configs

--- RUN_benchmark_block_coder.sh
+++ RUN_benchmark_block_coder.sh
@@ -1,14 +1,26 @@
-./build/linux/benchmark_parallel_encoder ./test_config/134217728_m_100_1_8_8_16_32.csv ./results
+./build/linux/benchmark_block_coder ./test_config/4194304_m_100_1_8_8_16_32.csv ./results
 sleep 10
-./build/linux/benchmark_parallel_encoder ./test_config/1073741824_g_100_1_8_8_16_32.csv ./results
+./build/linux/benchmark_block_coder ./test_config/33554432_m_100_1_8_8_16_32.csv ./results
 sleep 10
-./build/linux/benchmark_parallel_encoder ./test_config/104857600_m_100_1_8_8_16_32.csv ./results
+./build/linux/benchmark_block_coder ./test_config/.DS_Store ./results
 sleep 10
-./build/linux/benchmark_parallel_encoder ./test_config/536870912_m_100_1_8_8_16_32.csv ./results
+./build/linux/benchmark_block_coder ./test_config/16777216_m_100_1_8_8_16_32.csv ./results
 sleep 10
-./build/linux/benchmark_parallel_encoder ./test_config/268435456_m_100_1_8_8_16_32.csv ./results
+./build/linux/benchmark_block_coder ./test_config/268435456_m_100_1_8_8_16_32.csv ./results
 sleep 10
-./build/linux/benchmark_parallel_encoder ./test_config/1048576_m_100_1_8_8_16_32.csv ./results
+./build/linux/benchmark_block_coder ./test_config/8388608_m_100_1_8_8_16_32.csv ./results
 sleep 10
-./build/linux/benchmark_parallel_encoder ./test_config/10485760_m_100_1_8_8_16_32.csv ./results
+./build/linux/benchmark_block_coder ./test_config/10485760_m_100_1_8_8_16_32.csv ./results
+sleep 10
+./build/linux/benchmark_block_coder ./test_config/536870912_m_100_1_8_8_16_32.csv ./results
+sleep 10
+./build/linux/benchmark_block_coder ./test_config/134217728_m_100_1_8_8_16_32.csv ./results
+sleep 10
+./build/linux/benchmark_block_coder ./test_config/104857600_m_100_1_8_8_16_32.csv ./results
+sleep 10
+./build/linux/benchmark_block_coder ./test_config/67108864_m_100_1_8_8_16_32.csv ./results
+sleep 10
+./build/linux/benchmark_block_coder ./test_config/1073741824_g_100_1_8_8_16_32.csv ./results
+sleep 10
+./build/linux/benchmark_block_coder ./test_config/1048576_m_100_1_8_8_16_32.csv ./results
 sleep 10
